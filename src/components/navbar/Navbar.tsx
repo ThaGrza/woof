baeb28e386168d5ca8767246c9fb665643efee14
ThaGrza <andrewgriswold7@gmail.com>
2022-06-22 23:33:23 -0400

    skeleton structure

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css'
+
+function Navbar() {
+  return (
+    <div className='nav-container'>
+      <a href="#" className='nav-title-link'>
+        <h1 className='nav-title'>Woofer</h1>
+      </a>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/searchbar/searchbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function searchbar() {
+  return (
+    <button title='Enter Your City' className='searchbar'></button>
+  )
+}
+
+export default searchbar;
